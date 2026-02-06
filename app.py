5ce8a42f9aa165e2510e419ef3a7998d5febc552
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2026-02-06 05:23:42 +0000

    add rag api with chromadb

---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+import chromadb
+import ollama
+import uuid
+import os 
+import logging 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+MODEL_NAME = os.getenv("MODEL_NAME", "tinyllama")
+logging.info(f"Using Model: {MODEL_NAME}")
+
+app = FastAPI()
+chroma = chromadb.PersistentClient(path="./db")
+collection = chroma.get_or_create_collection("docs")
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/query")
+def query(q: str):
+    logging.info(f"/query asked: {q}")
+    
+    results = collection.query(query_texts=[q], n_results= 1)
+    context = results["documents"][0][0] if results["documents"] else ""
+
+    answer = ollama.generate(
+        model=MODEL_NAME,
+        prompt=f"Context:\n{context}\n\nQuestion: {q}\n\nAnswer clearly and concisely:"
+    )
+
+    return {"answer": answer['response']}
+
+@app.post("/add")
+def add(text: str):
+    logging.info(f"/add received new text (id will be generated)")
+    try:
+        doc_id = str(uuid.uuid4())
+
+        collection.add(documents = [text], ids=[doc_id])
+
+        return {"status": "success", "message": "Content added to knowledge base", "id": doc_id}
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
