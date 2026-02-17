2725f3344841e3998416c3561210538e7e0fcaf6
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2026-02-17 03:01:05 +0000

    add nextwork doc in doc folder

--- semantic_test.py
+++ semantic_test.py
@@ -10,7 +10,17 @@ def test_kubernetes_query():
     assert "container" in data["answer"].lower(), "Answer should mention container"
     print("Kubernetes Query Test Passed. Answer:", data["answer"])
 
+def test_nextwork_query():
+    response = requests.post("http://localhost:8000/query?q=What+is+Nextwork?") 
+    
+    print(response.status_code)
+    assert response.status_code == 200
+    data = response.json()
+    assert "platform" in data["answer"].lower(), "Answer should mention platform"
+    print("Nextwork Query Test Passed. Answer:", data["answer"])
+
 
 if __name__ == "__main__":
     test_kubernetes_query()
+    test_nextwork_query()
     print("All tests passed successfully.")
